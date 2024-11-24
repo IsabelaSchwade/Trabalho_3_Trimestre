2a0f88a114da9bc649be0284dbff5809030d0dab
IsabelaSchwade <isabelaschwade@gmail.com>
2024-11-24 13:32:57 -0300

    inicio do visualizar

--- index.php
+++ index.php
@@ -1,11 +1,37 @@
+<?php
+require_once __DIR__."/vendor/autoload.php"; // carrega as classes e dependencias uma unica vez
+
+$livros= livro::findall();// pegando todas as festas do banco e armazenando na variavel $festa
+// utiliza o findall pq pega todas as festas
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Página de Livros</title>
 </head>
 <body>
-    <h1>Testes</h1>
+
+<table>
+    <tr>
+        <td>Nome</td>
+    </tr>
+    <?php
+    foreach($livros as $livro){ // cria um loop para percorrer todas as festas, cada festa se torna na variavel $festa
+        //para cada festa cria uma linha <tr> e varias colunas <td> onde coloca o nome, o endereço, etc
+        echo "<tr>";
+        echo "<td>{$livro->getNomeLivro()}</td>";
+        echo "<td>{$festa->getimagemLivro()}</td>";
+        echo "<td>
+                <a href='formEdit.php?idFesta={$festa->getIdLivro()}'>Editar</a>
+                <a href='excluir.php?idFesta={$festa->getIdLivro()}'>Excluir</a> 
+             </td>";
+        echo "</tr>";
+    }
+    ?>
+</table>
+<a href='formCad.php'>Adicionar Livro</a>
 </body>
 </html>
---
+++ src/Livro.php
@@ -0,0 +1,73 @@
+<?php
+
+class Livro implements ActiveRecord{
+
+    private int $idLivro;
+    
+    public function __construct(private string $nomeLivro,private string $imagemLivro){
+    }
+
+    public function setIdLivro(int $idLivro):void{
+        $this->idLivro = $idLivro;
+    }
+
+    public function getIdLivro():int{
+        return $this->idLivro;
+    }
+
+    public function setNomeLivro(string $nomeLivro):void{
+        $this->nomeLivro = $nomeLivro;
+    }
+
+    public function getNomeLivro():string{
+        return $this->nomeLivro;
+    }
+
+    public function setimagemLivro(string $imagemLivro):void{
+        $this->imagemLivro = $imagemLivro;
+    }
+
+    public function getimagemLivro():string{
+        return $this->imagemLivro;
+    }
+
+
+    public function save():bool{
+        $conexao = new MySQL();
+        if(isset($this->idLivro)){
+            $sql = "UPDATE livro SET nomeLivro = '{$this->nomeLivro}' ,imagemLivro = '{$this->imagemLivro}' WHERE idLivro = {$this->idLivro}";
+        }else{
+            $sql = "INSERT INTO livro (nomeLivro, imagemLivro) VALUES ('{$this->nomeLivro}','{$this->imagemLivro}')";
+        }
+        return $conexao->executa($sql);
+        
+    }
+    public function delete():bool{
+        $conexao = new MySQL();
+        $sql = "DELETE FROM livro WHERE idLivro = {$this->idLivro}";
+        return $conexao->executa($sql);
+    }
+
+    public static function find($idLivro):Livro{
+        $conexao = new MySQL();
+        $sql = "SELECT * FROM livro WHERE idLivro = {$idLivro}";
+        $resultado = $conexao->consulta($sql);
+        $f = new livro($resultado[0]['nomeLivro'],$resultado[0]['imagemLivro']);
+        $f->setIdLivro($resultado[0]['idLivro']);
+        return $f;
+    }
+    public static function findall():array{
+        $conexao = new MySQL();
+        $sql = "SELECT * FROM livro";
+        $resultados = $conexao->consulta($sql);
+        $livros = array();
+        foreach($resultados as $resultado){
+            $f = new livro($resultado['nomeLivro'],$resultado['imagemLivro']);
+            $f->setIdLivro($resultado['idLivro']);
+            $festas[] = $f;
+        }
+        return $livros;
+    }
+
+    
+}
